13a152f64cd23af41fd7c3835e540985ddd4efbc
daniveloper <develored@gmail.com>
2019-03-02 20:38:50 -0500

    cambiado logo login y favicon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'http://api.box593.com/app_dev.php/',
+    API_URL: 'https://api.box593.com/app_dev.php/',
     CHAT_URL: 'ws://api.box593.com/wss2/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
     production: false,
-    API_URL: 'http://api.box593.com/app_dev.php/',
+    API_URL: 'https://api.box593.com/app_dev.php/',
     CHAT_URL: 'wss://api.box593.com/wss2/'
 };
